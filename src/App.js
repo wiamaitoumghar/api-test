65c04c6b42911d6a5208bde131ae6d02bfd7b524
wiamaitoumghar <wiam20wini@gmail.com>
2023-02-23 23:33:43 +0100

    ssss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React,{useState,useEffect}from 'react' 
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:4000/vip').then((res) => setData( res.data))
+    
+}, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+      {
+        data.map((vip,index)=>
+        <li key={index}>
+          <p>{vip.vname} </p> 
+          <p>{vip.vpassword} </p> 
+          <p>{vip.vflags} </p> 
+          <p>{ new Date(vip.enddate*1000).toDateString()} </p> 
+         </li>
+      
+       
+       
+        )
+      }
+      </ul>
+        
     </div>
   );
 }
